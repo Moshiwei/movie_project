394b78bd28c231dc1b5d352606d96edb537f0755
Moshiwei <1194317170@qq.com>
2017-10-15 18:06:03 +0800

    完成个人用户页面搭建

--- app/home/views.py
+++ app/home/views.py
@@ -6,14 +6,39 @@ from flask import render_template, redirect, url_for
 def index():
     return render_template('home/index.html')
 
+
 @home.route('/login/')
 def login():
     return render_template('home/login.html')
+
 
 @home.route('/logout/')
 def logout():
     return redirect(url_for('home.login'))
 
+
 @home.route('/register/')
 def register():
     return render_template('home/register.html')
+
+
+@home.route('/user/')
+def user():
+    return render_template('home/user.html')
+
+
+@home.route('/user/pwd/')
+def pwd():
+    return render_template('home/pwd.html')
+
+@home.route('/user/comments/')
+def comments():
+    return render_template('home/comments.html')
+
+@home.route('/user/loginlog/')
+def loginlog():
+    return render_template('home/loginlog.html')
+
+@home.route('/user/moviecol/')
+def moviecol():
+    return render_template('home/moviecol.html')
