2362705683a0c5a82bedcc20f4f399dadd7fc3ae
Moshiwei <1194317170@qq.com>
2017-10-24 22:24:32 +0800

    完成管理员登录页面搭建

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,5 +1,14 @@
 from . import admin
+from flask import redirect, render_template, url_for
 
 @admin.route('/')
 def index():
     return "<h1 style='color:green'>this is admin</h1>"
+
+@admin.route('/login/')
+def login():
+    return render_template('admin/login.html')
+
+@admin.route('/logout/')
+def logout():
+    return redirect(url_for('admin.login'))
